41c2a7e1903d95cf9538b58bbab05f37af029568
Mini Model <minimodel@metamodel.me>
2013-06-23 04:16:07 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsfilter_text/languages/it/tl_metamodel_filtersetting.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/it/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-04T02:35:43+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'Inizia con il termine di ricerca';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'Finisce con termine di ricerca';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'Ricerca esatta';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Tipo di ricerca';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Ricerca di parti di testo.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['text']        = 'Filtro testo';
