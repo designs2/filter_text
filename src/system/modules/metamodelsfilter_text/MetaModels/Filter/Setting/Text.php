6d67f77da596745a4aa41117407fd5fd055c3aea
David Molineus <mail@netzmacht.de>
2014-10-31 15:00:54 +0100

    switch to cca coding standards

--- src/system/modules/metamodelsfilter_text/MetaModels/Filter/Setting/Text.php
+++ src/system/modules/metamodelsfilter_text/MetaModels/Filter/Setting/Text.php
@@ -31,187 +31,195 @@ use MetaModels\FrontendIntegration\FrontendFilterOptions;
  */
 class Text extends SimpleLookup
 {
-	/**
-	 * Overrides the parent implementation to always return true, as this setting is always optional.
-	 *
-	 * @return bool true if all matches shall be returned, false otherwise.
-	 */
-	public function allowEmpty()
-	{
-		return true;
-	}
+    /**
+     * Overrides the parent implementation to always return true, as this setting is always optional.
+     *
+     * @return bool true if all matches shall be returned, false otherwise.
+     */
+    public function allowEmpty()
+    {
+        return true;
+    }
 
-	/**
-	 * Overrides the parent implementation to always return true, as this setting is always available for FE filtering.
-	 *
-	 * @return bool true as this setting is always available.
-	 */
-	public function enableFEFilterWidget()
-	{
-		return true;
-	}
+    /**
+     * Overrides the parent implementation to always return true, as this setting is always available for FE filtering.
+     *
+     * @return bool true as this setting is always available.
+     */
+    public function enableFEFilterWidget()
+    {
+        return true;
+    }
 
-	/**
-	 * Retrieve the filter parameter name to react on.
-	 *
-	 * @return string
-	 */
-	protected function getParamName()
-	{
-		if ($this->get('urlparam'))
-		{
-			return $this->get('urlparam');
-		}
+    /**
+     * Retrieve the filter parameter name to react on.
+     *
+     * @return string
+     */
+    protected function getParamName()
+    {
+        if ($this->get('urlparam')) {
+            return $this->get('urlparam');
+        }
 
-		$objAttribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
-		if ($objAttribute)
-		{
-			return $objAttribute->getColName();
-		}
+        $objAttribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
+        if ($objAttribute) {
+            return $objAttribute->getColName();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function prepareRules(IFilter $objFilter, $arrFilterUrl)
-	{
-		$objMetaModel  = $this->getMetaModel();
-		$objAttribute  = $objMetaModel->getAttributeById($this->get('attr_id'));
-		$strParamName  = $this->getParamName();
-		$strParamValue = $arrFilterUrl[$strParamName];
-		$strTextsearch = $this->get('textsearch');
+    /**
+     * {@inheritdoc}
+     */
+    public function prepareRules(IFilter $objFilter, $arrFilterUrl)
+    {
+        $objMetaModel  = $this->getMetaModel();
+        $objAttribute  = $objMetaModel->getAttributeById($this->get('attr_id'));
+        $strParamName  = $this->getParamName();
+        $strParamValue = $arrFilterUrl[$strParamName];
+        $strTextsearch = $this->get('textsearch');
 
-		// React on wildcard, overriding the search type.
-		if (strpos($strParamValue, '*') !== false)
-		{
-			$strTextsearch = 'exact';
-		}
+        // React on wildcard, overriding the search type.
+        if (strpos($strParamValue, '*') !== false) {
+            $strTextsearch = 'exact';
+        }
 
-		// Type of search.
-		switch ($strTextsearch)
-		{
-			case 'beginswith':
-				$strWhat = $strParamValue . '%';
-				break;
-			case 'endswith':
-				$strWhat = '%' . $strParamValue;
-				break;
-			case 'exact':
-				$strWhat = $strParamValue;
-				break;
-			default:
-				$strWhat = '%' . $strParamValue . '%';
-				break;
-		}
+        // Type of search.
+        switch ($strTextsearch) {
+            case 'beginswith':
+                $strWhat = $strParamValue . '%';
+                break;
+            case 'endswith':
+                $strWhat = '%' . $strParamValue;
+                break;
+            case 'exact':
+                $strWhat = $strParamValue;
+                break;
+            default:
+                $strWhat = '%' . $strParamValue . '%';
+                break;
+        }
 
-		if ($objAttribute && $strParamName && $strParamValue)
-		{
+        if ($objAttribute && $strParamName && $strParamValue) {
 
-			$objQuery = \Database::getInstance()->prepare(sprintf(
-				'SELECT id FROM %s WHERE %s LIKE ?',
-				$this->getMetaModel()->getTableName(),
-				$objAttribute->getColName()
-			))
-				->execute(str_replace(array('*', '?'), array('%', '_'), $strWhat));
+            $objQuery = \Database::getInstance()->prepare(sprintf(
+                'SELECT id FROM %s WHERE %s LIKE ?',
+                $this->getMetaModel()->getTableName(),
+                $objAttribute->getColName()
+            ))
+                ->execute(str_replace(array('*', '?'), array('%', '_'), $strWhat));
 
-			$arrIds = $objQuery->fetchEach('id');
+            $arrIds = $objQuery->fetchEach('id');
 
-			$objFilter->addFilterRule(new StaticIdList($arrIds));
-			return;
-		}
+            $objFilter->addFilterRule(new StaticIdList($arrIds));
 
-		$objFilter->addFilterRule(new StaticIdList(null));
-	}
+            return;
+        }
 
+        $objFilter->addFilterRule(new StaticIdList(null));
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getParameters()
-	{
-		return ($strParamName = $this->getParamName()) ? array($strParamName) : array();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getParameters()
+    {
+        return ($strParamName = $this->getParamName()) ? array($strParamName) : array();
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getParameterFilterNames()
-	{
-		if (($strParamName = $this->getParamName()))
-		{
-			return array(
-				$strParamName => ($this->get('label') ? $this->get('label') : $this->getParamName())
-			);
-		}
-		return array();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getParameterFilterNames()
+    {
+        if (($strParamName = $this->getParamName())) {
+            return array(
+                $strParamName => ($this->get('label') ? $this->get('label') : $this->getParamName())
+            );
+        }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getParameterFilterWidgets(
-		$arrIds,
-		$arrFilterUrl,
-		$arrJumpTo,
-		FrontendFilterOptions $objFrontendFilterOptions
-	)
-	{
-		// If defined as static, return nothing as not to be manipulated via editors.
-		if (!$this->enableFEFilterWidget())
-		{
-			return array();
-		}
+        return array();
+    }
 
-		$arrReturn                     = array();
-		$GLOBALS['MM_FILTER_PARAMS'][] = $this->getParamName();
+    /**
+     * {@inheritdoc}
+     */
+    public function getParameterFilterWidgets(
+        $arrIds,
+        $arrFilterUrl,
+        $arrJumpTo,
+        FrontendFilterOptions $objFrontendFilterOptions
+    ) {
+        // If defined as static, return nothing as not to be manipulated via editors.
+        if (!$this->enableFEFilterWidget()) {
+            return array();
+        }
 
-		// Address search.
-		$arrCount  = array();
-		$arrWidget = array(
-			'label'     => array(
-				($this->get('label') ? $this->get('label') : $this->getParamName()),
-				'GET: ' . $this->getParamName()
-			),
-			'inputType' => 'text',
-			'count'     => $arrCount,
-			'showCount' => $objFrontendFilterOptions->isShowCountValues(),
-			'eval'      => array(
-				'colname'  => $this->getMetaModel()->getAttributeById($this->get('attr_id'))->getColname(),
-				'urlparam' => $this->getParamName(),
-				'template' => $this->get('template'),
-			)
-		);
+        $arrReturn = array();
+        $this->addFilterParam($this->getParamName());
 
-		// Add filter.
-		$arrReturn[$this->getParamName()] =
-			$this->prepareFrontendFilterWidget($arrWidget, $arrFilterUrl, $arrJumpTo, $objFrontendFilterOptions);
-		return $arrReturn;
-	}
+        // Address search.
+        $arrCount  = array();
+        $arrWidget = array(
+            'label'     => array(
+                ($this->get('label') ? $this->get('label') : $this->getParamName()),
+                'GET: ' . $this->getParamName()
+            ),
+            'inputType' => 'text',
+            'count'     => $arrCount,
+            'showCount' => $objFrontendFilterOptions->isShowCountValues(),
+            'eval'      => array(
+                'colname'  => $this->getMetaModel()->getAttributeById($this->get('attr_id'))->getColname(),
+                'urlparam' => $this->getParamName(),
+                'template' => $this->get('template'),
+            )
+        );
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getParameterDCA()
-	{
-		$objAttribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
+        // Add filter.
+        $arrReturn[$this->getParamName()] =
+            $this->prepareFrontendFilterWidget($arrWidget, $arrFilterUrl, $arrJumpTo, $objFrontendFilterOptions);
 
-		$arrLabel = array(
-			($this->get('label') ? $this->get('label') : $objAttribute->getName()),
-			'GET: ' . $this->get('urlparam')
-		);
+        return $arrReturn;
+    }
 
-		return array(
-			$this->getParamName() => array
-			(
-				'label'     => $arrLabel,
-				'inputType' => 'text',
-				'eval'      => array(
-					'urlparam' => $this->get('urlparam'),
-					'template' => $this->get('template')
-				)
-			)
-		);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getParameterDCA()
+    {
+        $objAttribute = $this->getMetaModel()->getAttributeById($this->get('attr_id'));
+
+        $arrLabel = array(
+            ($this->get('label') ? $this->get('label') : $objAttribute->getName()),
+            'GET: ' . $this->get('urlparam')
+        );
+
+        return array(
+            $this->getParamName() => array
+            (
+                'label'     => $arrLabel,
+                'inputType' => 'text',
+                'eval'      => array(
+                    'urlparam' => $this->get('urlparam'),
+                    'template' => $this->get('template')
+                )
+            )
+        );
+    }
+
+    /**
+     * Add Param to global filter params array.
+     *
+     * @param string $param Name of filter param.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    private function addFilterParam($param)
+    {
+        $GLOBALS['MM_FILTER_PARAMS'][] = $param;
+    }
 }
