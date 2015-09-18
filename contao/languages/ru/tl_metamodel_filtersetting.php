5a31d04f3d237b8183af7a824ff6cbcd4cccf38b
Mini Model <minimodel@metamodel.me>
2015-09-18 10:33:01 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-12-18T08:14:18+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['beginswith'] = 'начинается критерием поиска';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['endswith']   = 'оканчивается критерием поиска';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['references']['exact']      = 'точный поиск';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['0']          = 'Тип поиска';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['textsearch']['1']          = 'Искать части текста.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['text']        = 'Текстовый фильтр';
+
